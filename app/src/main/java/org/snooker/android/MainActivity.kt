38a55ad3b07e697af7ba6cea682b9780b0e0fac7
Victor Kropp <victor.kropp@gmail.com>
2017-04-15 22:29:40 +0200

    pull to refresh

--- app/src/main/java/org/snooker/android/MainActivity.kt
+++ app/src/main/java/org/snooker/android/MainActivity.kt
@@ -1,11 +1,13 @@
 package org.snooker.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
+import org.snooker.api.Match
 
 class MainActivity : AppCompatActivity() {
     private val matchesListAdapter = MatchesListAdapter(this)
@@ -20,9 +22,24 @@ class MainActivity : AppCompatActivity() {
         matches_list.adapter = matchesListAdapter
         matches_list.layoutManager = LinearLayoutManager(this)
 
-        launch(UI) {
-            matchesListAdapter.setMatches(application.repository.matches())
-            matchesListAdapter.notifyDataSetChanged()
+        update()
+
+        swipe.setOnRefreshListener {
+            update()
         }
+    }
+
+    private fun update() {
+        launch(UI) {
+            matchesListAdapter.setMatches(application.repository.matches(), application.repository.rounds())
+            matchesListAdapter.notifyDataSetChanged()
+            swipe.isRefreshing = false
+        }
+    }
+
+    fun onMatchClicked(match: Match) {
+        val intent = Intent(this, MatchActivity::class.java)
+        intent.putExtra("id", match.id)
+        startActivity(intent)
     }
 }
