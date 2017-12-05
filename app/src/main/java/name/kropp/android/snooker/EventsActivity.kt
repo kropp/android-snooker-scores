994259b2f7787a03a18be9bb9220b6d036d6bb54
Victor Kropp <victor.kropp@gmail.com>
2017-12-05 22:20:05 +0100

    drawer layout

--- app/src/main/java/name/kropp/android/snooker/EventsActivity.kt
+++ app/src/main/java/name/kropp/android/snooker/EventsActivity.kt
@@ -1,29 +1,24 @@
 package name.kropp.android.snooker
 
-import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v4.app.ActivityOptionsCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.text.format.DateFormat
 import android.util.Log
-import android.util.Pair
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_events.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.run
 import name.kropp.android.snooker.api.Event
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
-import java.text.SimpleDateFormat
-import java.time.Duration
 import java.util.*
+
 
 class EventsActivity : AppCompatActivity() {
     private lateinit var events: List<Event>
@@ -37,16 +32,22 @@ class EventsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
 
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val drawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
+
         events_list.adapter = eventsAdapter
         events_list.layoutManager = LinearLayoutManager(this)
 
         // show data from cache immediately
         update(true)
         // and request update to show as soon as it is ready
-        update()
+//        update()
 
         swipe.setColorSchemeResources(R.color.colorPrimaryDark, R.color.colorPrimaryLight)
-        swipe.setDistanceToTriggerSync(20)
+        swipe.setDistanceToTriggerSync(200)
         swipe.setOnRefreshListener {
             update()
         }
@@ -89,7 +90,7 @@ class EventsActivity : AppCompatActivity() {
     fun onEventClicked(event: Event, eventNameView: View, eventDatesView: View) {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("id", event.id)
-        val sceneTransitionAnimation = ActivityOptions.makeSceneTransitionAnimation(this, Pair(eventNameView, "eventName"), Pair(eventDatesView, "eventDates"))
-        startActivity(intent, sceneTransitionAnimation.toBundle())
+//        val sceneTransitionAnimation = ActivityOptions.makeSceneTransitionAnimation(this, Pair(eventNameView, "eventName"), Pair(eventDatesView, "eventDates"))
+        startActivity(intent/*, sceneTransitionAnimation.toBundle()*/)
     }
 }
