eaf64e7e0ae1263018cec588878298753d035a8b
Victor Kropp <victor.kropp@gmail.com>
2018-01-20 21:48:39 +0100

    add live indicator

--- app/src/main/java/name/kropp/android/snooker/EventsListAdapter.kt
+++ app/src/main/java/name/kropp/android/snooker/EventsListAdapter.kt
@@ -2,7 +2,11 @@ package name.kropp.android.snooker
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.TextView
 import name.kropp.android.snooker.api.Event
 import name.kropp.android.snooker.api.Match
 import java.util.*
@@ -21,7 +25,7 @@ class EventsListAdapter(private val activity: EventsActivity) : RecyclerView.Ada
         liveMatchesCount = ongoingMatchesByRound.values.sumBy { it.size + 1 }
     }
 
-    override fun getItemCount() = events.size + liveMatchesCount
+    override fun getItemCount() = if (events.isEmpty()) 0 else events.size + liveMatchesCount
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
@@ -31,7 +35,7 @@ class EventsListAdapter(private val activity: EventsActivity) : RecyclerView.Ada
         }
     }
 
-    fun matchAt(position: Int): Match? {
+    private fun matchAt(position: Int): Match? {
         var pos = position
         for (matches in ongoingMatchesByRound.values) {
             pos -= 1
@@ -43,7 +47,7 @@ class EventsListAdapter(private val activity: EventsActivity) : RecyclerView.Ada
         return null
     }
 
-    fun roundAt(position: Int): String? {
+    private fun roundAt(position: Int): String? {
         var pos = position
         for ((key, value) in ongoingMatchesByRound) {
             if (pos == 0) {
@@ -63,8 +67,21 @@ class EventsListAdapter(private val activity: EventsActivity) : RecyclerView.Ada
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when (viewType) {
         MATCH_VIEWTYPE -> MatchViewHolder(activity, LayoutInflater.from(parent.context).inflate(R.layout.matches_list_item, parent, false))
-        ROUND_VIEWTYPE -> RoundViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.matches_list_round, parent, false))
+        // all matches on main screen are live
+        ROUND_VIEWTYPE -> RoundViewHolder(createLiveRoundView(parent))
+        ROUND_LIVE_VIEWTYPE -> RoundViewHolder(createLiveRoundView(parent))
         EVENT_VIEWTYPE -> EventViewHolder(activity, LayoutInflater.from(parent.context).inflate(R.layout.events_list_item, parent, false))
         else -> null
+    }
+
+    private fun createLiveRoundView(parent: ViewGroup): View {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.matches_list_round_live, parent, false)
+        val live = view.findViewById<TextView>(R.id.label_live)
+        live.animation = AlphaAnimation(0.7f, 1f).apply {
+            duration = 2000
+            repeatMode = Animation.REVERSE
+            repeatCount = 2
+        }
+        return view
     }
 }
--- app/src/main/java/name/kropp/android/snooker/OngoingMatchesViewModel.kt
+++ app/src/main/java/name/kropp/android/snooker/OngoingMatchesViewModel.kt
@@ -3,31 +3,20 @@ package name.kropp.android.snooker
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
 import name.kropp.android.snooker.api.Match
 
 
 class OngoingMatchesViewModel(application: Application) : AndroidViewModel(application) {
-    private lateinit var matches: MutableLiveData<Pair<List<Match>, Map<Long,String>>>
+    val matches = MutableLiveData<Pair<List<Match>, Map<Long,String>>>()
 
-    fun live(): MutableLiveData<Pair<List<Match>, Map<Long,String>>> {
-        if (!::matches.isInitialized) {
-            matches = MutableLiveData()
+    suspend fun refresh() {
+        val repository = getApplication<SnookerApplication>().repository
 
-            refresh()
-        }
-        return matches
-    }
+        val ongoingMatches = repository.ongoingMatches().await()
 
-    private fun refresh() {
-        launch(UI) {
-            val repository = getApplication<SnookerApplication>().repository
-            val ongoingMatches = repository.ongoingMatches(false).await()
-            if (!ongoingMatches.isEmpty()) {
-                val rounds = repository.rounds(ongoingMatches.first().eventId).await()
-                matches.value = ongoingMatches to rounds
-            }
+        if (ongoingMatches.isNotEmpty()) {
+            val rounds = repository.rounds(ongoingMatches.first().eventId).await()
+            matches.value = ongoingMatches to rounds
         }
     }
 }
